81bc6057b7bf31741b3e1c4f4d351efd00cdb502
maryam <maryam1998.mostafa@gmail.com>
2022-04-24 20:32:48 +0200

    my prev-last commit: updated layout still some issues in callback (;-;) sad

--- Layout.py
+++ Layout.py
@@ -3,59 +3,51 @@ from dash import html
 import dash_bootstrap_components as dbc
 import dash
 from Components import *
-#from run import app
 import Callback
 
 
 app.layout = dbc.Container([
 
-    ### row 1 cards of big number  ================================================================================
+    # row 1 cards of big number  ================================================================================
     dbc.Row([
-        dbc.Col(mainTitle_desc(), width=6),
-        dbc.Col([bigCards()], width=5 ),
+        dbc.Col(mainTitle_desc(), width=8),
+        dbc.Col([bigCards()], width=4),
 
         ], justify='evenly'),
     html.Br(),
-    # row 2 ==================================================================================================
-    dbc.Row([
-        dbc.Col(yearMonthCard(), width=11, className="card"),
 
-    ],justify='evenly'),# style={"margin-top": "2%"}),
-    html.Br(),
-    # slider row 3=============================================================================================
+    # row 2 slider and drop down =============================================================================================
     dbc.Row([
-        dbc.Col(main_years_slider() , width=12, className="card"),
+        dbc.Col(filters(), width=12, className="card"),
 
-    ],justify='around'),# style={"margin-top": "2%"}),
+    ],justify='around'),
     html.Br(),
+
+    # row 3 ==================================================================================================
+    dbc.Row([
+        dbc.Col(graphes1(), width=6),
+        dbc.Col(graphes2(), width=6),
+
+    ],justify='evenly', className='div'),
+    html.Br(),
+
     # row 4 ==================================================================================================
     dbc.Row([
-        dbc.Col(mostPopTalk_card(), width=6, className="card"),
-        dbc.Col(mostDiscussionTopic(), width=5, className="card"),
+        dbc.Col(wordCloud(), width=4,),
+        dbc.Col(duration_card(), width=8),
 
-    ],justify='evenly'),
-    html.Br(),#style={"margin-top": "2%","margin-left": "15px", 'layout.autosize': 'true'}, ),  # style={"margin-top": "20px","margin-right": "15px"},),
+    ]),
+    html.Br(),
+
     # row 5 ==================================================================================================
     dbc.Row([
-        dbc.Col(most_popular_occupation(), width=6, className="card"),
-        dbc.Col(mostPopSpreaker_Card(), width=5, className="card"),
-
-    ],justify='evenly'),# style={"margin-top": "2%","margin-left": "15px"}),  # style={"margin-top": "20px","margin-right": "15px"}),
-    html.Br(),
-    # row 6 ==================================================================================================
-    dbc.Row([
-        dbc.Col(duration_card(), width=6, className="card"),
-        dbc.Col(wordCloud(), width=5, className="card"),
-
-    ], justify='evenly')
-
-], #style = {'background-image':"url('https://media.istockphoto.com/vectors/white-brick-wall-textured-background-vector-id1094376824?k=20&m=1094376824&s=612x612&w=0&h=X2573vpqJFd9F_sqFN9Xb-fKhe88ZpPLuYta4srrLQI=')" })
-style = {# 'background-image':"url('https://encrypted-tbn0.gstatic.com/images?q=tbn
-    # :ANd9GcStzFM4eqUXlfeLkXO_C32jljkxMUl3wlH53Ywmx1368KNbLVcSjfuVfcYm-FkC1zi55V8&usqp=CAU')"
+        dbc.Col(yearMonthCard(), width=12),
+    ]),
+],
+style = {
          'backgroundColor' : '#ECECEC',
-         "padding-top": "2%", "padding-left": "2%","padding-right": "2%", "padding-bottom":'2%'
+         "padding-top": "2%","padding-left": "2%","padding-right": "2%", "padding-bottom":'2%'
          }, fluid= True)
-#app.run_server(port=5080)
-#style = {'backgroundColor':'#FAFAFA'})
+
 if __name__ == '__main__':
     app.run_server(port=5050,debug=True)
--- StaticCharts.py
+++ StaticCharts.py
@@ -3,24 +3,24 @@ import pandas as pd
 from matplotlib import pyplot as plt
 import plotly.graph_objects as go
 from dataPreperation import get_dataFrame
-from wordcloud import WordCloud
+# from wordcloud import WordCloud
 import seaborn as sns
 
 # try again
 data = get_dataFrame()
-#palette = sns.color_palette("RdGy",10).as_hex()
+# palette = sns.color_palette("RdGy",10).as_hex()
 # print(palette)
 
 
-red_pallete = ['#8e0015','#9b1422','#a7272f','#b43b3c','#c04e49','#cd6256','#d97563','#e68970','#f29c7d','#ffb08a']
-
-
+red_pallete = ['#8e0015', '#9b1422', '#a7272f', '#b43b3c', '#c04e49', '#cd6256', '#d97563', '#e68970', '#f29c7d',
+               '#ffb08a']
 
 
 def mostPopularTalk(df):
     most_popular_talk_by_views = df.sort_values(by="views", ascending=False)
     fig = px.bar(most_popular_talk_by_views[:10], y="title", x="views", text='title', orientation='h',
-                 color='title', color_discrete_sequence = red_pallete, template="simple_white")
+                 color='title', color_discrete_sequence=red_pallete,
+                 template="simple_white")
     fig.update_traces(textposition='inside')
     fig.update_yaxes(showticklabels=False)
     fig.update(layout_showlegend=False)
@@ -34,7 +34,7 @@ def mostPopularTalk(df):
 def popular_talk_pieChart(df):
     popular_df = df[['title', 'comments']].sort_values('comments', ascending=False)
     fig = px.pie(popular_df.iloc[:5], values='comments', names='title', hole=.2,
-                 color_discrete_sequence=red_pallete, template="simple_white")  # height = 500)
+                 color_discrete_sequence=red_pallete, template="simple_white")
     fig.update_layout(
         legend=dict(xanchor="center", orientation="h", x=0.5, y=-0.2),
         margin=dict(l=20, r=20, t=20, b=20))
@@ -58,12 +58,15 @@ def most_popular_speaker(df):
 
 
 def most_popular_speaker_occupation(df):
+
     top_occupations = df.sort_values(by="views", ascending=False)
     top_occupations.drop_duplicates(subset=['speaker_occupation'], inplace=True)
+
     fig = px.bar(top_occupations[:10], x="views", y="speaker_occupation",
                  orientation='h', color='speaker_occupation', color_discrete_sequence=red_pallete,
                  labels={'views': 'TED talks Views', 'speaker_occupation': 'Speaker Occupation'},
                  text='speaker_occupation', template="simple_white")
+
     fig.update_traces(textposition='inside')
     fig.update_yaxes(showticklabels=False)
     fig.update(layout_showlegend=False)
@@ -73,6 +76,27 @@ def most_popular_speaker_occupation(df):
     return fig
 
 
+def durationBar_chart(df, at):
+    flag = False
+    if at == "tab1_id":
+        flag = False
+    elif at == "tab2_id":
+        flag = True
+    duration = df[['title', 'duration']].sort_values('duration', ascending=flag)
+    fig = px.bar(duration.iloc[:10], x='title', y='duration', color='duration',
+                 color_continuous_scale=['#ffb08a', '#f29c7d', '#e68970', '#d97563', '#cd6256', '#c04e49', '#b43b3c',
+                                         '#a7272f', '#9b1422', '#8e0015'], labels={'title': 'Title of talk'},
+                 template='simple_white', text='title')
+
+    fig.update_traces(textposition='inside')
+    fig.update(layout_coloraxis_showscale=False)
+    fig.update_xaxes(showticklabels=False)
+    fig.update_layout(margin=dict(l=20, r=20, t=20, b=20),)
+
+    return fig
+
+
+# area chart of year and month
 def new_scatter_line(df, at):
     trace, x_axis = None, ''
     if at == "year_id":
@@ -96,37 +120,35 @@ def new_scatter_line(df, at):
 
     layout = go.Layout(
         xaxis=dict(title=x_axis),
-        yaxis=dict(title='Frequency'),
-        template='simple_white',
+        yaxis=dict(title='Number of talks'),
+        template='plotly_white',
         margin=dict(l=20, r=20, t=20, b=20)
     )
     fig = go.Figure(data=trace, layout=layout)
     return fig
+###################################### keep this ##################################################
+# #word cloud chart not working with me because of liberary
+# def create_wordCloud_img():
+#     tag_dict = pd.Series([x for _list in data.tags for x in _list])
+#     count = {}
+#     for word in tag_dict:
+#         count.setdefault(word, 0)
+#         count[word] += 1
+#
+#     list_count = list(count.items())
+#     list_count.sort(key=lambda i: i[1], reverse=True)
+#     for i in list_count:
+#        print(i[0], ':', i[1])
 
-#word cloud
-def create_wordCloud_img():
-    tag_dict = pd.Series([x for _list in data.tags for x in _list])
-    count = {}
-    for word in tag_dict:
-        count.setdefault(word, 0)
-        count[word] += 1
-
-    list_count = list(count.items())
-    list_count.sort(key=lambda i: i[1], reverse=True)
-    for i in list_count:
-       print(i[0], ':', i[1])
-
-
-    # #### this part not working with me ####
-    plt.subplots(figsize = (8,8))
-    wordcloud = WordCloud(
-        background_color = 'white',
-        colormap = 'gist_heat',
-        width = 800,
-        height = 800,max_words=11100
-    ).generate(str(list_count))
-    plt.imshow(wordcloud) # image show
-    plt.axis('off') # to off the axis of x and y
-    plt.savefig("wordcloud.png")
-    plt.show()
-
+# plt.subplots(figsize = (8,8))
+# wordcloud = WordCloud(
+#     background_color = 'white',
+#     colormap = 'gist_heat',
+#     width = 800,
+#     height = 800,max_words=11100
+# ).generate(str(list_count))
+# plt.imshow(wordcloud) # image show
+# plt.axis('off') # to off the axis of x and y
+# plt.savefig("wordcloud.png")
+# plt.show()
+#############################################################################################
